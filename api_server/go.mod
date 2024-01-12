93d9b51e2c19db64c4cb78f0c65225d927c613c7
yumarun <yuta.wota.0048@gmail.com>
2024-01-13 06:10:34 +0900

    google login server

--- api_server/go.mod
+++ api_server/go.mod
@@ -2,4 +2,4 @@ module yumarun/TM_apiserver
 
 go 1.21.0
 
-require github.com/go-sql-driver/mysql v1.7.1 // indirect
+require github.com/go-sql-driver/mysql v1.7.1
--- api_server/tm_apiserver.go
+++ api_server/tm_apiserver.go
@@ -13,20 +13,23 @@ import (
 )
 
 type dbEntry struct {
-	id int
-	token string 
-	name string
+	id     int
+	token  string
+	name   string
 	rating int
 }
-
 
 var mysqlPassword string
 
 // GET (token -> name, rating)
 func getNameAndRating(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
+	w.Header().Set("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,UPDATE,OPTIONS")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+
 	name := "x"
 	rating := -1
-	
+
 	body := make([]byte, r.ContentLength)
 	r.Body.Read(body)
 
@@ -37,9 +40,9 @@ func getNameAndRating(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 	defer db.Close()
-	
+
 	rows, err := db.Query("SELECT * FROM users_test where token = ?", token)
-	var usersResult []dbEntry 
+	var usersResult []dbEntry
 	for rows.Next() {
 		user := dbEntry{}
 		if err := rows.Scan(&user.id, &user.token, &user.name, &user.rating); err != nil {
@@ -51,10 +54,8 @@ func getNameAndRating(w http.ResponseWriter, r *http.Request) {
 		name = usersResult[0].name
 		rating = usersResult[0].rating
 	}
-	fmt.Fprintf(w, name + "\n" + strconv.Itoa(rating))
+	fmt.Fprintf(w, name+"\n"+strconv.Itoa(rating))
 }
-
-
 
 // GET (token, name -> fail or success)
 func setRating(w http.ResponseWriter, r *http.Request) {
@@ -82,8 +83,6 @@ func setRating(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "fin")
 }
 
-
-
 // GET (token, name -> fail or success)
 func setName(w http.ResponseWriter, r *http.Request) {
 	body := make([]byte, r.ContentLength)
@@ -106,9 +105,70 @@ func setName(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "fin")
 }
 
+// POST (token, name)
+func registerAccount(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
+	w.Header().Set("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,UPDATE,OPTIONS")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+	body := make([]byte, r.ContentLength)
+	r.Body.Read(body)
+	args := strings.Split(string(body), "%20")
+	fmt.Println("args length: ", len(args), " 0: ", args[0])
+	db, err := sql.Open("mysql", "root:"+mysqlPassword+"@tcp(localhost:3306)/TM")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	ins, err := db.Prepare("INSERT INTO users_test (token,name,rating) VALUES(?,?,?)")
+	if err != nil {
+		panic(err)
+	}
+	ins.Exec(args[0], args[1], 1500)
+
+	fmt.Fprintf(w, "fin")
+}
+
+// POST (name)
+func checkDeplicateName(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
+	w.Header().Set("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,UPDATE,OPTIONS")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+	name := make([]byte, r.ContentLength)
+	r.Body.Read(name)
+	db, err := sql.Open("mysql", "root:"+mysqlPassword+"@tcp(localhost:3306)/TM")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT * FROM users_test where name = ?", name)
+	if err != nil {
+		panic(err)
+	}
+	count := 0
+	for rows.Next() {
+		user := dbEntry{}
+		if err := rows.Scan(&user.id, &user.token, &user.name, &user.rating); err != nil {
+			log.Fatal(err)
+		}
+		count = count + 1
+	}
+	defer rows.Close()
+
+	if count == 0 {
+		fmt.Fprintf(w, "ok")
+	} else {
+		fmt.Fprintf(w, "bad")
+	}
+
+}
+
 func main() {
 	fmt.Println("api server start!")
-	
+
 	f, err := os.Open("MYSQL_PASSWORD.txt")
 	if err != nil {
 		panic(err)
@@ -123,15 +183,16 @@ func main() {
 	}
 
 	mysqlPassword = string(buf[:n-1])
-	
+
 	db, err := sql.Open("mysql", "root:"+mysqlPassword+"@tcp(localhost:3306)/TM")
 	if err != nil {
 		panic(err)
 	}
 
 	defer db.Close()
-	
+
 	rows, err := db.Query("SELECT * FROM users_test")
+	defer rows.Close()
 	var userResult []dbEntry
 	for rows.Next() {
 		user := dbEntry{}
@@ -140,8 +201,8 @@ func main() {
 		}
 		userResult = append(userResult, user)
 	}
-	for _, u := range(userResult) {
-		fmt.Println("id: ", u.id, " token: ", u.token, " name: ", u.name, " rating: ", u.rating)
+	for _, u := range userResult {
+		fmt.Println("id: ", u.id, " token: ", u.token[0:7], " name: ", u.name, " rating: ", u.rating)
 	}
 
 	if err != nil {
@@ -153,8 +214,9 @@ func main() {
 	http.HandleFunc("/api/SetName/", setName)
 	http.HandleFunc("/api/SetRating/", setRating)
 	http.HandleFunc("/api/NameAndRating/", getNameAndRating)
-	log.Fatal(http.ListenAndServe(":8080", nil))
-	
+	http.HandleFunc("/api/Register/", registerAccount)
+	http.HandleFunc("/api/CheckDeplicateName/", checkDeplicateName)
+	log.Fatal(http.ListenAndServeTLS(":8080", "/etc/letsencrypt/archive/tmapiserver.yumarun.net/fullchain1.pem", "/etc/letsencrypt/archive/tmapiserver.yumarun.net/privkey1.pem", nil))
 
 	fmt.Println("fin")
 }
