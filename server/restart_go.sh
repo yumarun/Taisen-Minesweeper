27a194db0b82b9a76b30e9966c9655251954bfe7
yumarun <yuta.wota.0048@gmail.com>
2024-01-22 06:52:27 +0000

    fix restart_go.sh

--- server/restart_go.sh
+++ server/restart_go.sh
@@ -1,9 +1,6 @@
-#PATH=/home/yumaru/.vscode-server/bin/0ee08df0cf4527e40edc9aa28f4b5bd38bbff2b2/bin/remote-cli:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/yumaru/.dotnet/tools:/usr/local/go/bin:/usr/local/go/bin
-
 process_id=$(pgrep -o main)
 if [ -z "$process_id" ]; then
 	echo "restart go run ."
-	cd /home/yumaru/TM/server
-	#go run /home/yumaru/TM/server/main.go >> aaa.log &
+	/usr/local/go/bin/go run /home/yumaru/TM/server/main.go >> aaa.log &
 	echo "finn"
 fi
